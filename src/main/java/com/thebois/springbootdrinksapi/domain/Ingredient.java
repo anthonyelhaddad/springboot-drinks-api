d9ec3e8e46794777e5814332e0f53cafe3bdbd10
Michel Haddad <michhadd1@gmail.com>
2021-05-11 22:42:17 +0300

    Feat: add first iteration of Ingredient entity

---
+++ src/main/java/com/thebois/springbootdrinksapi/domain/Ingredient.java
@@ -0,0 +1,94 @@
+package com.thebois.springbootdrinksapi.domain;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity
+public class Ingredient {
+    @Id
+    @SequenceGenerator(
+            name = "ingredient_sequence",
+            sequenceName = "ingredient_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "ingredient_sequence"
+    )
+    @Column(
+            name = "id"
+    )
+    private Long id;
+
+    @Column(
+            name = "name",
+            nullable = false
+    )
+    private String name;
+
+    @Column(
+            name = "description",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String description;
+
+    @Column(
+            name = "type",
+            nullable = false
+    )
+    private String type;
+
+    public Ingredient() {
+
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Ingredient(String name, String description, String type) {
+        this.name = name;
+        this.description = description;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
