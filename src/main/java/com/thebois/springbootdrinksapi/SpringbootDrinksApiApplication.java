141aec648133a98dd97f5d0039a4b072344ef8d6
anthonyelhaddad <72256204+anthonyelhaddad@users.noreply.github.com>
2021-05-11 21:06:45 +0200

    Add files via upload

---
+++ src/main/java/com/thebois/springbootdrinksapi/SpringbootDrinksApiApplication.java
@@ -0,0 +1,13 @@
+package com.thebois.springbootdrinksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDrinksApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDrinksApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thebois/springbootdrinksapi/SpringbootDrinksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thebois.springbootdrinksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDrinksApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
