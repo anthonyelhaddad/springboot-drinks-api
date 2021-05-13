4e7c6902d0fa14daf6cee708829a1d039c0fde0f
Michel Haddad <michhadd1@gmail.com>
2021-05-13 21:22:23 +0300

    Docs: update docs

--- src/main/java/com/thebois/springbootdrinksapi/api/rest/docs/SpringFoxConfig.java
+++ src/main/java/com/thebois/springbootdrinksapi/api/rest/docs/SpringFoxConfig.java
@@ -2,8 +2,10 @@ package com.thebois.springbootdrinksapi.api.rest.docs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -15,6 +17,18 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Drinks REST API Documentation")
+                .description("Drinks - REST API")
+                .termsOfServiceUrl("github")
+                .license("The bois")
+                .licenseUrl("")
+                .version("1.0")
                 .build();
     }
 }
