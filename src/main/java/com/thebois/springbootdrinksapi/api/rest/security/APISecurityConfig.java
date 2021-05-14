2ba411d20089fd6a7daea189265d07f6cc6456da
Michel Haddad <michhadd1@gmail.com>
2021-05-14 11:11:14 +0300

    Feat: add api token security

---
+++ src/main/java/com/thebois/springbootdrinksapi/api/rest/security/APISecurityConfig.java
@@ -0,0 +1,51 @@
+package com.thebois.springbootdrinksapi.api.rest.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
+public class APISecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${API_KEY_HEADER}")
+    private String principalRequestHeader;
+
+    @Value("${API_KEY_VALUE}")
+    private String principalRequestValue;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        APIKeyAuthFilter filter = new APIKeyAuthFilter(principalRequestHeader);
+        filter.setAuthenticationManager(new AuthenticationManager() {
+
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                String principal = (String) authentication.getPrincipal();
+                if (!principalRequestValue.equals(principal))
+                {
+                    throw new BadCredentialsException("The API key was not found or not the expected value.");
+                }
+                authentication.setAuthenticated(true);
+                return authentication;
+            }
+        });
+        httpSecurity.
+                antMatcher("/api/**").
+                csrf().disable().
+                sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeRequests().antMatchers(HttpMethod.GET, "/**").permitAll()
+                .and().addFilter(filter).authorizeRequests().anyRequest().authenticated();
+    }
+
+}
